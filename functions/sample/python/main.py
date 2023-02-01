c9bae1abda8ea3c115294e6e28a81dfe467a86d7
Paul Wanjohi <119477356+devwanjohi@users.noreply.github.com>
2023-02-01 17:00:29 +0300

    Update main.py

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -33,3 +33,20 @@ def main(param_dict):
         return {"error": err}
 
     return {"dbs": client.all_dbs()}
+
+
+try:
+        client = Cloudant.iam(
+            account_name=param_dict["COUCH_USERNAME"],
+            api_key=param_dict["IAM_API_KEY"],
+            connect=True,
+        )
+        print(f"Databases: {client.all_dbs()}")
+    except CloudantException as cloudant_exception:
+        print("unable to connect")
+        return {"error": cloudant_exception}
+    except (requests.exceptions.RequestException, ConnectionResetError) as err:
+        print("connection error")
+        return {"error": err}
+
+    return {"dbs": client.all_dbs()}
