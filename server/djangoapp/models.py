8448d25f44d3486f308df3359752a5e1c182023c
devwanjohi <pwanjohi164@gmail.com>
2023-02-02 03:03:51 -0500

    developeds django view and rest api

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -79,7 +79,7 @@ class CarDealer:
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
 class DealerReview:
-    def __init__(self, dealership, name, purchase, id, review, purchase_date, car_make, car_model, car_year):
+    def __init__(self, dealership, name, purchase, id, review, purchase_date, car_make, car_model, car_year, sentiment):
         self.dealership = dealership
         self.name = name
         self.purchase = purchase
@@ -89,6 +89,7 @@ class DealerReview:
         self.car_make = car_make
         self.car_model = car_model
         self.car_year = car_year
+        self.sentiment = sentiment
 
     def __str__(self):
         return "Review: " + self.review
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
+from django.http.response import Http404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
-from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf
+from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf, post_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
 import logging
 import json
+import random
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -73,19 +75,28 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
+    context = {}
     if request.method == "GET":
         url = "https://us-south.functions.appdomain.cloud/api/v1/web/e2c27cb3-f03d-49b6-a540-db62d7091f1c/dealership-package/get-dealership.json"
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url)
+        get_states = []
+        for dealer in dealerships:
+            get_states.append(dealer.st)
+        just_states = set(get_states)
+
+        context['dealers'] = dealerships
+        context['states'] = sorted(just_states)
         # Concat all dealer's short name
-        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
         # Return a list of dealer short name
-        return HttpResponse(dealer_names)
+        return render(request, 'djangoapp/index', context)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
 def get_dealer_details(request, dealer_id):
+    context = {}
     if request.method == "GET":
         url = "https://us-south.functions.appdomain.cloud/api/v1/web/e2c27cb3-f03d-49b6-a540-db62d7091f1c/dealership-package/get-review.json"
         # Get dealers from the URL
@@ -98,4 +109,40 @@ def get_dealer_details(request, dealer_id):
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
+def add_review(request, dealer_id):
+    url = "https://us-south.functions.appdomain.cloud/api/v1/web/e2c27cb3-f03d-49b6-a540-db62d7091f1c/dealership-package/post-review-sequence"
+    context = {}
+    # Check if user is  authenticated
+    if request.user.is_authenticated:
+        try:
+            car = get_object_or_404(CarModel, pk=request.POST.get('car'))
 
+            review = {
+                'id': random.randint(100,200),
+                'name': request.user.name,
+                'dealership': dealer_id,
+                'review': request.POST.get('review'),
+                'purchase': request.POST.get('purchase', False),
+                'purchase_date': request.POST.get('purchase_date', None),
+                'car_make': car.car_make.name,
+                'car_model': car.name,
+                'car_year': car.car_year.year
+            }
+        except Http404:
+            review = {
+                'id': random.randint(100,200),
+                'name': request.user.name,
+                'dealership': dealer_id,
+                'review': request.POST.get('review'),
+                'purchase': request.POST.get('purchase', False)
+            }
+        json_payload = {"review": review}
+
+        r = post_request(url, json_payload=json_payload, dealer_id=dealer_id)
+        if r == 200:
+            # context['status'] = 'Posted successfully!'
+            print('Posteed successfully!')
+        else:
+            context['status'] = '[{}] Something went wrong on the server.'.format(r)
+        review_view = redirect('djangoapp:dealer_details', dealer_id=dealer_id)
+    return review_view
